abede4dc319f7c3a8ca7a23a067acc2cef7f353a
maxymlyskov <raypember02@gmail.com>
2023-04-05 15:15:57 +0300

    Made navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/home/Home'
-import Signup from './pages/signup/Signup'
+import Signup from './pages/signup/Singup'
 import Login from './pages/login/Login'
+import Navbar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
     <div className="App">
       <Switch>
         <Route exact path='/'>
