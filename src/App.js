dc08d101ca5c33a6a4168dd1c99e1bd5f26c8518
maxymlyskov <raypember02@gmail.com>
2023-04-05 15:09:20 +0300

    Created pages & routes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Signup from './pages/signup/Signup'
+import Login from './pages/login/Login'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/signup'>
+          <Signup/>
+        </Route>
+        <Route exact path='/login'>
+          <Login/>
+        </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/signup/Singup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Signup.module.css'
+
+function Singup() {
+  return (
+    <div>Singup</div>
+  )
+}
+
+export default Singup
