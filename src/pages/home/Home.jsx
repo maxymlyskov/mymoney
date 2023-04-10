26f1afe677483ffbd5a5650af3fba35198f4f271
maxymlyskov <raypember02@gmail.com>
2023-04-10 13:15:38 +0300

    Added useCollection usage.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,13 +4,19 @@ import styles from "./Home.module.css";
 // components
 import TransactionForm from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { error, documents } = useCollection("transactions");
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
