7194e5223f5ac03c2e8149e5500885961995ee7e
maxymlyskov <raypember02@gmail.com>
2023-04-10 13:02:33 +0300

    Added useFirestore usage.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,16 @@ import styles from "./Home.module.css";
 
 // components
 import TransactionForm from "./TransactionForm";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
