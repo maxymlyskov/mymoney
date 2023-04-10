030e01ce655f263b510f1f3814ff72759172ef39
maxymlyskov <raypember02@gmail.com>
2023-04-10 10:04:10 +0300

    Added previous auth on load.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ import Signup from "./pages/signup/Singup";
 import Login from "./pages/login/Login";
 import Navbar from "./components/NavBar/NavBar";
 import { AuthContextProvider } from "./context/AuthContext";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
-    <AuthContextProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div className="App">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    </AuthContextProvider>
+    <>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <div className="App">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      )}
+    </>
   );
 }
 
