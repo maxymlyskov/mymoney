2cfef59b8a90365f9a2b4364aa07fa775e0ea9df
maxymlyskov <raypember02@gmail.com>
2023-04-10 12:47:30 +0300

    Added timestamp to transaction adding.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,23 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAsDnqinstibe69dGyqL4KDkvK82U5OwP0",
-    authDomain: "mymoneywallet-8b50b.firebaseapp.com",
-    projectId: "mymoneywallet-8b50b",
-    storageBucket: "mymoneywallet-8b50b.appspot.com",
-    messagingSenderId: "956802613729",
-    appId: "1:956802613729:web:cae061742ce7e831bb12a0"
-  };
+  apiKey: "AIzaSyAsDnqinstibe69dGyqL4KDkvK82U5OwP0",
+  authDomain: "mymoneywallet-8b50b.firebaseapp.com",
+  projectId: "mymoneywallet-8b50b",
+  storageBucket: "mymoneywallet-8b50b.appspot.com",
+  messagingSenderId: "956802613729",
+  appId: "1:956802613729:web:cae061742ce7e831bb12a0",
+};
 
 // init firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 // init services
-const projectFirestore = firebase.firestore()
-const projectAuth = firebase.auth()
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFirestore, projectAuth }
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
